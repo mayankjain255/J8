28b99ce503a9717dc87d03c26751e3c855a8f2f1
MayankJain-Quo <mayank.jain@quovantis.com>
2020-02-18 23:26:02 +0530

    Removed annotation | updated .gitignore file

--- DesignPatterns/src/main/java/com/designpatterns/structural/decorator/Circle.java
+++ DesignPatterns/src/main/java/com/designpatterns/structural/decorator/Circle.java
@@ -3,7 +3,6 @@ package com.designpatterns.structural.decorator;
 
 public class Circle implements Shape {
 
-   @Override
    public void draw() {
       System.out.println("Shape: Circle");
    }
--- DesignPatterns/src/main/java/com/designpatterns/structural/decorator/Rectangle.java
+++ DesignPatterns/src/main/java/com/designpatterns/structural/decorator/Rectangle.java
@@ -3,7 +3,6 @@ package com.designpatterns.structural.decorator;
 
 public class Rectangle implements Shape {
 
-   @Override
    public void draw() {
       System.out.println("Shape: Rectangle");
    }
